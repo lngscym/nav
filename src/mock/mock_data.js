e01ada2801c743d92649820d5f1517a9014e4ace
lngscym <chii@sina.cn>
2026-02-03 21:17:33 +0800

    chore: 更新导航数据 - 2026/2/3 21:17:32

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -129,13 +129,6 @@ export const mockData = {
       "order": 6,
       "sites": [
         {
-          "id": "linuxdo",
-          "name": "Linux.do",
-          "url": "https://linux.do",
-          "description": "Linux与开源技术社区",
-          "icon": "/sitelogo/linux.do.ico"
-        },
-        {
           "id": "nodeseek",
           "name": "NodeSeek",
           "url": "https://www.nodeseek.com",
@@ -148,13 +141,6 @@ export const mockData = {
           "url": "https://www.52pojie.cn/",
           "description": "软件安全与破解技术论坛",
           "icon": "/sitelogo/www.52pojie.cn.ico"
-        },
-        {
-          "id": "linux-do",
-          "name": "Linux.do",
-          "url": "https://linux.do",
-          "description": "Linux技术社区，Peace and Love",
-          "icon": "/sitelogo/linux.do.ico"
         }
       ]
     },
